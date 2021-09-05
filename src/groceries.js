75096c8356e2561a27df5057bc0bf8682c74045d
sinuhem23 <sinuhem23@gmail.com>
2021-09-05 00:47:59 -0400

    need to add map to display list of groceries correctly

--- src/groceries.js
+++ src/groceries.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './grocery.css';
 /*  
 Conditionally render the grocery items based on whether or not they were purchased (ok to have hard coded values for isPurchased)
 Add some style to your app
@@ -21,8 +22,13 @@ export class Groceries extends Component {
 
     this.setState({
       isPurchased: true,
-      groceries: [this.state.item, this.state.units, this.state.quantity],
+      // groceries: [this.state.item, this.state.units, this.state.quantity],
     });
+    if (e) {
+      this.state.groceries.push([this.state.item]);
+      this.state.groceries.push([this.state.units]);
+      this.state.groceries.push([this.state.quantity]);
+    }
   };
 
   // Make an array of 3 objects.
@@ -32,7 +38,14 @@ export class Groceries extends Component {
   }
 
   // Make inputs so that new items can be added
-  inputs() {}
+  inputs = () => {
+    console.log('here');
+    this.setState({
+      item: '',
+      units: '',
+      quantity: 0,
+    });
+  };
 
   render() {
     console.log('form data: ', this.state);
@@ -40,12 +53,13 @@ export class Groceries extends Component {
     // Then render the item, quantity and units (12 pack, 1lb, 2 liters, etc.)
     return (
       <div>
-        <form className='form' onSubmit={this.handleSubmit}>
+        <form id='input_form' onSubmit={this.handleSubmit}>
           <lable htmlFor='item'>Add Grocery Item</lable>
           <input
             id='item'
             onChange={this.handleChange}
             value={this.state.item}
+            type='text'
           />
 
           <lable htmlFor='quantity'>Quantity</lable>
@@ -53,6 +67,7 @@ export class Groceries extends Component {
             id='quantity'
             onChange={this.handleChange}
             value={this.state.quantity}
+            type='text'
           />
 
           <lable htmlFor='units'>Units</lable>
@@ -60,14 +75,28 @@ export class Groceries extends Component {
             id='units'
             onChange={this.handleChange}
             value={this.state.units}
+            type='text'
           />
 
           <button onClick={() => this.componentDidMount()}>Submit</button>
         </form>
 
+        {/* Add Another Grocery */}
+        <button value='Reset data' onClick={() => this.inputs()}>
+          Add Another Item
+        </button>
+
+        {/* Display List */}
         <div>
           <h2>Your Grocery List</h2>
+
+          {/* Need a Map to iterate */}
           <h4>{this.state.groceries}</h4>
+
+          <h4>Item: {this.state.groceries[0]}</h4>
+          <h4>Quantity: {this.state.groceries[2]}</h4>
+
+          <h4>Unit: {this.state.groceries[1]}</h4>
         </div>
 
         {/* <button onClick={() => this.inputs()}>Input a grocery</button>
